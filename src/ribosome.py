ad460ba42d168b5980823ad15467b39db9707787
Dongyeong Kim <42883224+Dongyeongkim@users.noreply.github.com>
2019-12-26 23:15:40 +0900

    Update ribosome.py

--- src/ribosome.py
+++ src/ribosome.py
@@ -1,4 +1,5 @@
 import os
+import torch
 import src.Genetic as Genetic
 from bindsnet.network import nodes
 from bindsnet.network import Network
@@ -17,7 +18,7 @@ def Translate_Into_Networks(input_N,Shape,Output_N,Weight):
         Output_Layer = nodes.LIFNodes(n=Output_N, refrac=0, traces=True)
         network.add_layer(layer=Input_Layer, name="Input_Layer")
         network.add_layer(layer=Output_Layer, name="Output_Layer")
-        Decoded_List = []
+        Decoded_List = []; Decoded_DNA_List = []
         for j in range(len(Gene_List[i])):
             Decoded_Gene=Gene_List[i][j].split('-')
             if(Decoded_Gene[3]=='F'):
@@ -36,84 +37,113 @@ def Translate_Into_Networks(input_N,Shape,Output_N,Weight):
                 else:
                     print("THE GENOTYPE VALUE IS UNVALID")
                     raise ValueError
-        for k in range(len(Decoded_List)):
-            if(k==len(Decoded_List)):
-                break
+            Decoded_DNA_List.append(Decoded_List)
 
-            for l in range(k+1, len(Decoded_List)):
-                if ((Decoded_List[k][0] == Decoded_List[l][0])and(Decoded_List[k][1] == Decoded_List[l][1]))== True:
+        Decoded_RNA_List: list = Decoded_DNA_List.copy()
 
-                    if Decoded_List[k][2] == 0:
-                        Decoded_List.remove(Decoded_List[l])
+        for decoded_dna_idx, decoded_dna in enumerate(Decoded_DNA_List):
+            for k in range(len(decoded_dna) - 1):
+                for l in range(k + 1, len(decoded_dna) - 3):
+                    is_source = decoded_dna[k][0] == decoded_dna[l][0]
+                    is_target = decoded_dna[k][1] == decoded_dna[l][1]
+                    if is_source and is_target:
 
-                    elif Decoded_List[k][2] == 1:
-                        if Decoded_List[l][2] < 1:
-                            Decoded_List.remove(Decoded_List[k])
+                        if decoded_dna[k][2] == 0:
+                            Decoded_RNA_List[decoded_dna_idx].remove(decoded_dna[l])
+
+                        elif decoded_dna[k][2] == 1:
+                            if decoded_dna[l][2] < 1:
+                                Decoded_RNA_List[decoded_dna_idx].remove(decoded_dna[k])
+                            else:
+                                Decoded_RNA_List[decoded_dna_idx].remove(decoded_dna[l])
+
+                        elif decoded_dna[k][2] == 2:
+                            if decoded_dna[l][2] < 2:
+                                Decoded_RNA_List[decoded_dna_idx].remove(decoded_dna[k])
+                            else:
+                                Decoded_RNA_List[decoded_dna_idx].remove(decoded_dna[l])
+
+                        elif decoded_dna[k][2] == 3:
+                            if decoded_dna[l][2] < 3:
+                                Decoded_RNA_List[decoded_dna_idx].remove(decoded_dna[k])
+                            else:
+                                Decoded_RNA_List[decoded_dna_idx].remove(decoded_dna[l])
+
+                        elif decoded_dna[k][2] == 4:
+                            if decoded_dna[l][2] >= 4:
+                                Decoded_RNA_List[decoded_dna_idx].remove(decoded_dna[l])
+                            else:
+                                Decoded_RNA_List[decoded_dna_idx].remove(decoded_dna[k])
+
                         else:
-                            Decoded_List.remove(Decoded_List[l])
-
-                    elif Decoded_List[k][2] == 2:
-                        if Decoded_List[l][2] < 2:
-                            Decoded_List.remove(Decoded_List[k])
-                        else:
-                            Decoded_List.remove(Decoded_List[l])
-
-                    elif Decoded_List[k][2] == 3:
-                        if Decoded_List[l][2] < 3:
-                            Decoded_List.remove(Decoded_List[k])
-                        else:
-                            Decoded_List.remove(Decoded_List[l])
-
-                    elif Decoded_List[k][2] == 4:
-                        if Decoded_List[l][2] >= 4:
-                            Decoded_List.remove(Decoded_List[l])
-                        else:
-                            Decoded_List.remove(Decoded_List[k])
+                            pass
 
                     else:
                         pass
 
-                else:
-                    pass
 
-        layer_list = {}
-        for m in range(len(Decoded_List)):
-            for n in range(len(Decoded_List)):
-                if Decoded_List[m][1] == Decoded_List[n][0]:
-                    if Decoded_List[n][2] == '0':
-                        layer_list[m] = nodes.IFNodes(n=1,traces=True)
-                    elif Decoded_List[n][2] == '1':
-                        layer_list[m] = nodes.LIFNodes(n=1,traces=True)
-                    elif Decoded_List[n][2] == '2':
-                        layer_list[m] = nodes.McCullochPitts(n=1,traces=True)
-                    elif Decoded_List[n][2] == '3':
-                        layer_list[m] = nodes.IzhikevichNodes(n=1,traces=True)
-                    elif Decoded_List[n][2] == '4':
-                        layer_list[m] = nodes.SRM0Nodes(n=1,traces=True)
-                    else:
-                        print("UNVALID GENO_NEURON CODE");raise ValueError
+        for Decoded_RNA in Decoded_RNA_List:
+            print(Decoded_RNA)
+            layer_list = {}
+            for m in range(len(Decoded_RNA)):
 
-                elif n == len(Decoded_List)-1:
-                    layer_list[m] = nodes.LIFNodes(n=1)
+                for n in range(m, len(Decoded_RNA)):
+                    if Decoded_RNA[m][1] == Decoded_RNA[n][0]:
+                        print(Decoded_RNA[m], Decoded_RNA[n])
+                        if Decoded_RNA[n][2] == 0:
+                            layer_list[Decoded_RNA[m][0]] = nodes.IFNodes(n=1, traces=True)
+                        elif Decoded_RNA[n][2] == 1:
+                            layer_list[Decoded_RNA[m][0]] = nodes.LIFNodes(n=1, traces=True)
+                        elif Decoded_RNA[n][2] == 2:
+                            layer_list[Decoded_RNA[m][0]] = nodes.McCullochPitts(n=1, traces=True)
+                        elif Decoded_RNA[n][2] == 3:
+                            layer_list[Decoded_RNA[m][0]] = nodes.IzhikevichNodes(n=1, traces=True)
+                        elif Decoded_RNA[n][2] == 4:
+                            layer_list[Decoded_RNA[m][0]] = nodes.SRM0Nodes(n=1, traces=True)
+                        else:
+                            print("UNVALID GENO_NEURON CODE");
+                            raise ValueError
 
-        for key_l in list(layer_list.keys()):
-            network.add_layer(layer=layer_list[key_l], name=str(key_l))
+                    elif n == len(Decoded_List) - 1:
+                        layer_list[Decoded_RNA[m][1]] = nodes.LIFNodes(n=1)
 
-        for key_ic in list(layer_list.keys()):
-            inpt_connection = Connection(source=Input_Layer, target=layer_list[key_ic], w=Weight)
-            network.add_connection(inpt_connection, source="Input_Layer", target=str(key_ic))
+            if not Decoded_RNA[len(Decoded_RNA) - 1][0] in layer_list:
+                if Decoded_RNA[len(Decoded_RNA) - 1][2] == 0:
+                    layer_list[Decoded_RNA[len(Decoded_RNA) - 1][0]] = nodes.IFNodes(n=1, traces=True)
+                elif Decoded_RNA[len(Decoded_RNA) - 1][2] == 1:
+                    layer_list[Decoded_RNA[len(Decoded_RNA) - 1][0]] = nodes.LIFNodes(n=1, traces=True)
+                elif Decoded_RNA[len(Decoded_RNA) - 1][2] == 2:
+                    layer_list[Decoded_RNA[len(Decoded_RNA) - 1][0]] = nodes.McCullochPitts(n=1, traces=True)
+                elif Decoded_RNA[len(Decoded_RNA) - 1][2] == 3:
+                    layer_list[Decoded_RNA[len(Decoded_RNA) - 1][0]] = nodes.IzhikevichNodes(n=1, traces=True)
+                elif Decoded_RNA[len(Decoded_RNA) - 1][2] == 4:
+                    layer_list[Decoded_RNA[len(Decoded_RNA) - 1][0]] = nodes.SRM0Nodes(n=1, traces=True)
 
-        for key_op in list(layer_list.keys()):
-            output_connection = Connection(source=layer_list[key_op], target=Output_Layer[key_op % len(Output_Layer)])
-            network.add_connection(output_connection, source=str(key_op), target="Output_" + str(key_op % len(Output_Layer)),
-                                   w=Weight)
+            for key_ic in list(layer_list.keys()):
+                print(layer_list[key_ic])
+                inpt_connection = Connection(source=Input_Layer, target=layer_list[key_ic],
+                                             w=Weight * torch.eye(layer_list[key_ic].n - 1))
+                network.add_connection(inpt_connection, source="Input_Layer", target=str(key_ic))
 
-        for o in range(len(Decoded_List)):
-            mid_connection = Connection(source=layer_list[Decoded_List[0]], target=layer_list[Decoded_List[1]],
-                                        wmin=-2, wmax=2, update_rule=MSTDP, norm=Weight)
-            network.add_connection(mid_connection, source=str(Decoded_List[0]), target=str(Decoded_List[1]))
+            for key_op in list(layer_list.keys()):
+                output_connection = Connection(source=layer_list[key_op], target=Output_Layer,
+                                               w=Weight * torch.eye(layer_list[key_op].n - 1), update_rule=MSTDP)
+                network.add_connection(output_connection, source=str(key_op), target="Output_" + str(key_op % Output_N))
+            print(layer_list)
+            for generating_protein in Decoded_RNA:
+                print(generating_protein)
+                mid_connection = Connection(source=layer_list[generating_protein[0]],
+                                            target=layer_list[generating_protein[1]],
+                                            wmin=-2, wmax=2, update_rule=MSTDP, norm=Weight)
+                network.add_connection(mid_connection, source=str(Decoded_List[0]), target=str(Decoded_List[1]))
 
-        network.save('Network/'+str(i)+'.pt')
+            for key_l in list(layer_list.keys()):
+                network.add_layer(layer=layer_list[key_l], name=str(key_l))
+
+            network.save('Network/' + str(i) + '.pt')
+
+
+
 
 
 
