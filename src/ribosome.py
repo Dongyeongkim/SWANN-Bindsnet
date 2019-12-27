4f21294769dd5e7fb88b10a6507980cd8f4dfbf0
Dongyeong Kim <42883224+Dongyeongkim@users.noreply.github.com>
2019-12-28 00:32:51 +0900

    Update ribosome.py

--- src/ribosome.py
+++ src/ribosome.py
@@ -1,6 +1,5 @@
 import os
-import torch
-import Genetic
+import src.Genetic as Genetic
 from bindsnet.network import nodes
 from bindsnet.network import Network
 from bindsnet.network.topology import Connection
@@ -11,14 +10,14 @@ def Translate_Into_Networks(input_N,Shape,Output_N,Weight):
     path = "gene/"; file_list = os.listdir(path)
     gene_file_check = [file for file in file_list if file.endswith(".txt")]
     if len(gene_file_check) == 0:
-        import startup
+        import src.startup
     Gene_List = Genetic.Read_Gene()
     for i in range(len(Gene_List)):
         network = Network()
         Input_Layer = nodes.Input(n=input_N, shape=Shape, traces=True)
-        output = nodes.LIFNodes(n=Output_N, refrac=0, traces=True)
+        out = nodes.LIFNodes(n=Output_N, refrac=0, traces=True)
         network.add_layer(layer=Input_Layer, name="Input_Layer")
-        network.add_layer(layer=output, name="Output Layer")
+        network.add_layer(layer=out, name="Output Layer")
         Decoded_List = []; Decoded_DNA_List = []
         for j in range(len(Gene_List[i])):
             Decoded_Gene=Gene_List[i][j].split('-')
@@ -44,7 +43,7 @@ def Translate_Into_Networks(input_N,Shape,Output_N,Weight):
 
         for decoded_dna_idx, decoded_dna in enumerate(Decoded_DNA_List):
             for k in range(len(decoded_dna) - 1):
-                for l in range(k + 1, len(decoded_dna) - 3):
+                for l in range(k + 1, len(decoded_dna)):
                     is_source = decoded_dna[k][0] == decoded_dna[l][0]
                     is_target = decoded_dna[k][1] == decoded_dna[l][1]
                     if is_source and is_target:
@@ -90,7 +89,6 @@ def Translate_Into_Networks(input_N,Shape,Output_N,Weight):
 
                 for n in range(m, len(Decoded_RNA)):
                     if Decoded_RNA[m][1] == Decoded_RNA[n][0]:
-                        print(Decoded_RNA[m], Decoded_RNA[n])
                         if Decoded_RNA[n][2] == 0:
                             layer_list[Decoded_RNA[m][0]] = nodes.IFNodes(n=1, traces=True)
                         elif Decoded_RNA[n][2] == 1:
@@ -102,7 +100,7 @@ def Translate_Into_Networks(input_N,Shape,Output_N,Weight):
                         elif Decoded_RNA[n][2] == 4:
                             layer_list[Decoded_RNA[m][0]] = nodes.SRM0Nodes(n=1, traces=True)
                         else:
-                            print("UNVALID GENO_NEURON CODE");
+                            print("UNVALID GENO_NEURON CODE")
                             raise ValueError
 
                     elif n == len(Decoded_List) - 1:
@@ -121,33 +119,24 @@ def Translate_Into_Networks(input_N,Shape,Output_N,Weight):
                     layer_list[Decoded_RNA[len(Decoded_RNA) - 1][0]] = nodes.SRM0Nodes(n=1, traces=True)
 
             for key_ic in list(layer_list.keys()):
-                print(layer_list[key_ic])
                 inpt_connection = Connection(source=Input_Layer, target=layer_list[key_ic],
-                                             w=Weight * torch.eye(80*80))
+                                             norm=Weight)
                 network.add_connection(inpt_connection, source="Input_Layer", target=str(key_ic))
 
             for key_op in list(layer_list.keys()):
-                output_connection = Connection(source=layer_list[key_op], target=output,
-                                               w=Weight * torch.eye(1*4), update_rule=MSTDP)
-                network.add_connection(output_connection, source=str(key_op), target="Output_" + str(key_op % Output_N))
+                output_connection = Connection(source=layer_list[key_op], target=out,
+                                               norm=Weight, update_rule=MSTDP)
+                network.add_connection(output_connection, source=str(key_op), target="Output Layer")
             print(layer_list)
             for generating_protein in Decoded_RNA:
                 print(generating_protein)
                 mid_connection = Connection(source=layer_list[generating_protein[0]],
                                             target=layer_list[generating_protein[1]],
                                             wmin=-2, wmax=2, update_rule=MSTDP, norm=Weight)
-                network.add_connection(mid_connection, source=str(Decoded_List[0]), target=str(Decoded_List[1]))
+                network.add_connection(mid_connection, source=str(generating_protein[0]), target=str(generating_protein[1]))
 
             for key_l in list(layer_list.keys()):
                 network.add_layer(layer=layer_list[key_l], name=str(key_l))
             network_list.append(network)
             network.save('Network/' + str(i) + '.pt')
     return network_list
-
-
-
-
-
-
-
-
