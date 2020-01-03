0821d711e40aadaee384a39b05ec0c45fcded817
Dongyeong Kim <42883224+Dongyeongkim@users.noreply.github.com>
2020-01-03 19:09:34 +0900

    Update main.py

--- src/main.py
+++ src/main.py
@@ -4,12 +4,12 @@ import Genetic
 import ribosome
 import random as rd
 
-
-Generation_num = 100#int(input("Input the number of testing generations"))
-
-for i in range(Generation_num):
+Generation_Num = int(input("Input Generation Number"))
+gene_list = []
+for i in range(Generation_Num):
     gene_list = Genetic.Read_Gene()
-
+    if(len(gene_list) == 0):
+        import startup
     gene_score_list = []
     selected_gene = []
     descendants_gene = []
@@ -19,18 +19,18 @@ for i in range(Generation_num):
     score_list = SNN.return_score(network_list,i)
     gene_score_list.append(score_list)
     selected_gene: list = NEAT.calc_and_select_gene(gene_score_list, gene_list)
-    gene_num: int = len(gene_list)/2
+    gene_num: int = int(len(gene_list)/2)
     for k in range(gene_num):
         cross1 = rd.choice(selected_gene)
         cross2 = rd.choice(selected_gene)
+        print(cross1,cross2)
         Agene1,Agene2 = NEAT.Crossover(cross1, cross2)
-        gene_list.remove(cross1)
-        gene_list.remove(cross2)
         descendants_gene.append(Agene1)
         descendants_gene.append(Agene2)
     for idx_gene,gene in enumerate(descendants_gene):
         descendant_mutated.append(NEAT.Mutate(gene,score_list[idx_gene],1000000))
     Genetic.Write_Gene(descendants_gene)
+        
 
 
 
